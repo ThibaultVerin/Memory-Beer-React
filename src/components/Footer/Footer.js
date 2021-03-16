f8a32911f900053181b60eee0f26a5ba3512e78f
Thibault Verin <thibaultverin@MacBook-Air-de-Thibault.local>
2021-03-16 23:04:46 +0100

    footer with responsive finished

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,12 +2,24 @@ import React from 'react';
 import { useHistory } from "react-router-dom";
 import './Footer.scss';
 import BurgerMenu from '../Footer/BurgerMenu';
+import linkedin from '../../data/pictures/linkedin.png';
+import github from '../../data/pictures/github.png';
 
 const Footer = () => {
 
     const history = useHistory();
 
-    const button = ['Home', 'Beer']
+    const button = ['Home', 'Beer'];
+    const icon = [{
+        name: 'github',
+        src: github,
+        url:'https://github.com/ThibaultVerin'
+     },
+     {
+        name: 'linkedin',
+        src: linkedin,
+        url:'https://www.linkedin.com/in/thibaultverin/'
+     }];
 
     const handleClick = (e) => {
 
@@ -22,6 +34,7 @@ const Footer = () => {
         }
     }
 
+    console.log(icon[0])
 
     return (
         <div className='footer-container-mobile'> 
@@ -29,6 +42,20 @@ const Footer = () => {
                 <input key={button} className='footer-button' type='button' value={button} onClick={(e) => handleClick(e)} />
             )}
             <BurgerMenu />
+            <div className='footer-container-desktop'>
+                <div className='footer-desktop-img'>
+                    {icon.map((icon) => 
+                        <a 
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            href={icon.url}
+                        >
+                            <img src={icon.src} alt={icon.name}/>
+                        </a>
+                    )}
+                </div>
+                <p>Copyright © 2021 - Thibault Verin - All Rights Reserve.</p>
+            </div>
         </div>
     )
 }

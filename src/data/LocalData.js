82f8553f8fb1e06ebb3593cf1d3804106afa0d96
Thibault Verin <thibaultverin@MacBook-Air-de-Thibault.local>
2021-03-17 14:23:46 +0100

    Set up single board component and beer cards on local data

--- src/data/LocalData.js
+++ src/data/LocalData.js
@@ -1,5 +1,20 @@
 import linkedin from './pictures/linkedin.png';
 import github from './pictures/github.png';
+import bottle1 from './cards/bouteille1.png';
+import bottle2 from './cards/bouteille2.png';
+import bottle3 from './cards/bouteille3.png';
+import bottle4 from './cards/bouteille4.png';
+import bottle5 from './cards/bouteille5.png';
+import bottle6 from './cards/bouteille6.png';
+import bottle7 from './cards/bouteille7.png';
+import bottle8 from './cards/bouteille8.png';
+
+
+// LINKS
+
+export const NavLink = ['Home', 'Ranking', 'About', 'Settings'];
+
+// FOOTER
 
 export const Icon = [{
     name: 'github',
@@ -12,14 +27,20 @@ export const Icon = [{
     url:'https://www.linkedin.com/in/thibaultverin/'
  }];
 
- export const NavLink = ['Home', 'Ranking', 'About', 'Settings'];
-
- export const FooterButton = ['Home', 'Beer'];
+ export const FooterButton = ['Home', 'Rules'];
 
  export const HomeButton = ['Single - Player', 'Multi - Player', 'Tutorial'];
+
+ // LEVEL SELECTION
  
  export const LevelButton = ['Sober', 'Drunk', 'Wasted'];
+
+ // NUMBER SELECTION
 
  export const NumberPlayer = ['2', '3', '4'];
 
  export const NumberButton = ['Back', 'Enter'];
+
+ // BEER CARDS
+
+ export const BeerCards = [bottle1, bottle2, bottle3, bottle4, bottle5, bottle6, bottle7, bottle8]

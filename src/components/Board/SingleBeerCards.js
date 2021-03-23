4b4fe695516a27d00088a4e7c9f2cb603a8ff9d4
Thibault Verin <thibaultverin@MacBook-Air-de-Thibault.home>
2021-03-24 00:26:17 +0100

    setting the classname shuffle on single board game

--- src/components/Board/SingleBeerCards.js
+++ src/components/Board/SingleBeerCards.js
@@ -2,51 +2,39 @@ import React, { useState, useEffect } from 'react';
 import './SingleBeerCards.scss';
 import logo from '../../data/pictures/Logo.png';
 
-const SingleBeerCards = ({ card, index }) => {
+const SingleBeerCards = ({ card, classname, handleClick }) => {
 
-    // console.log(card.id)
-
-    // console.log(card.id - 1 )
-    // console.log(index)
-    const [cardOpen, setCardOpen] = useState(false);
-    const [playerFirstChoice, setPlayerFirstChoice] = useState();
-    const [playerSecondChoice, setPlayerSecondChoice] = useState();
     const [match, setMatch] = useState(false);
 
     const [playerScore, setPlayerScore] = useState(0);
 
 
-    useEffect(() => {
-        matchingResult();
-    }, [playerSecondChoice]);
+    // /*eslint-disable */
+    // useEffect(() => {
+    //     matchingResult();
+    // }, [playerSecondChoice]);
+    // /*eslint-enable */
 
     const handleClassName = () => {
      }
 
-     const handleClick = (card) => {
-        setCardOpen(!cardOpen);
-        if (!playerFirstChoice) {
-            setPlayerFirstChoice(card.id);
-        } else {
-            setPlayerSecondChoice(card.id);
-        }
-    }
 
-    const matchingResult = () => {
-        if (playerFirstChoice !== undefined && playerSecondChoice !== undefined) {
-            if (playerFirstChoice === playerSecondChoice) {
-                setMatch(!match);
-                setPlayerScore(playerScore + 10);
-            }
-            setPlayerScore(playerScore - 10);
-        }
-        setMatch(!match);
-        setPlayerFirstChoice();
-        setPlayerSecondChoice();
-    }
+
+    // const matchingResult = () => {
+    //     if (playerFirstChoice !== undefined && playerSecondChoice !== undefined) {
+    //         if (playerFirstChoice === playerSecondChoice) {
+    //             setMatch(!match);
+    //             setPlayerScore(playerScore + 10);
+    //         }
+    //         setPlayerScore(playerScore - 10);
+    //     }
+    //     setMatch(!match);
+    //     setPlayerFirstChoice();
+    //     setPlayerSecondChoice();
+    // }
 
     return (
-        <div className='single-board-card-open' onClick={() => handleClick(card)}>
+        <div className={classname} onClick={() => handleClick(card)}>
             <img src={card.src} alt={card.name} id={card.id} />
             <img src={logo} alt ='logo' />
         </div>
--- src/components/Board/SingleBoard.js
+++ src/components/Board/SingleBoard.js
@@ -13,13 +13,20 @@ const SingleBoard = () => {
     const { userInfo } = useContext(UserContext);
 
     const [sortedBeerCards, setSortedBeerCards] = useState([]);
-
     const [playerScore, setPlayerScore] = useState(0);
+
+
+    const [cardOpen, setCardOpen] = useState(false);
+    const [playerFirstChoice, setPlayerFirstChoice] = useState();
+    const [playerSecondChoice, setPlayerSecondChoice] = useState();
+
+    const [classname, setClassname] = useState('single-board-card-close')
+    const [cardClicked, setCardClicked] = useState();
 
    
 
     useEffect(() => {
-            SortedCards();
+        SortedCards();
     }, []);
     
 
@@ -38,21 +45,44 @@ const SingleBoard = () => {
         shuffleArray(newBeerCards);
      }
 
-     function shuffleArray(array) {
+    function shuffleArray(array) {
         for (let i = array.length - 1; i > 0; i--) {
           let j = Math.floor(Math.random() * (i + 1));
           [array[i], array[j]] = [array[j], array[i]];
         }
         return array;
-      }
+    }
+
+    const handleClick = (card) => {
+        setCardClicked(card.id);
+        setCardOpen(!cardOpen);
+        handleClassname(card.id);
+        if (!playerFirstChoice) {
+            setPlayerFirstChoice(card.id);
+            console.log(`FirstCard : ${card.id}`);
+        } else {
+            setPlayerSecondChoice(card.id);
+            console.log(`SecondCard : ${card.id}`);
+        }
+    }
+
+    const handleClassname = (id) => {
+        console.log(id);
+        console.log(cardClicked);
+        if (cardOpen === true) {
+            setClassname('single-board-card-open');
+        } else {
+            setClassname('single-board-card-close');
+        }
+    }
 
 
     return (
         <div className='single-board-container'>
             {userInfo.name} : {playerScore}
             <div className='single-board-card-container'>
-                {sortedBeerCards.map((card, index) => 
-                    <SingleBeerCards key ={index} card={card} index={index} />
+                {sortedBeerCards.map((card) => 
+                    <SingleBeerCards key ={card.id} card={card} classname={classname} cardClicked={cardClicked} handleClick={handleClick} />
                 )}
             </div>
         </div>

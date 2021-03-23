459b173864224bb861cc79c136c116aee9178c2d
Thibault Verin <thibaultverin@MacBook-Air-de-Thibault.home>
2021-03-24 00:37:22 +0100

    Initialization of settings component

---
+++ src/components/Settings/Settings.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Settings.scss'
+
+
+const Settings = () => {
+    return (
+        <div className='settings-container'>
+            <div className='settings-'>
+                Guy Mauve Production
+            </div>
+        </div>
+    )
+}
+
+export default Settings;

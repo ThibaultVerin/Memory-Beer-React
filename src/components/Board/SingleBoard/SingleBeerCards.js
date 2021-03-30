be44eaa2d76a58dea1e1d4aab64e67ef6a2d865c
Thibault Verin <thibaultverin@MacBook-Air-de-Thibault.local>
2021-03-30 05:32:58 +0200

    Refactoring all css and responsive of single board

--- src/components/Board/SingleBoard/SingleBeerCards.js
+++ src/components/Board/SingleBoard/SingleBeerCards.js
@@ -4,7 +4,7 @@ import logo from '../../../data/pictures/Logo.png';
 
 const SingleBeerCards = ({ card, handleClick, pair, isPlayed, lockBoard }) => {
 
-    const [classname, setClassname] = useState('close');
+    const [classname, setClassname] = useState('single-board-card close');
     const [clicked, setClicked] = useState(false);
     const [match, setMatch] = useState(false);
     const [played, setPlayed] = useState(false);
@@ -31,13 +31,13 @@ const SingleBeerCards = ({ card, handleClick, pair, isPlayed, lockBoard }) => {
 
     const handleClassName = () => {
         if (clicked && played){
-            setClassname('open');
+            setClassname('single-board-card open');
             setClicked(false);
         }
         else if (clicked || match){
-            setClassname('open');
+            setClassname('single-board-card open');
         } else {
-            setClassname('close');
+            setClassname('single-board-card close');
             setClicked(false);
         }
     }
@@ -52,7 +52,7 @@ const SingleBeerCards = ({ card, handleClick, pair, isPlayed, lockBoard }) => {
 
     return (
         <div 
-            className={`single-board-card-${classname}`} 
+            className={classname} 
             onClick={() => Playing()}
         >
             <img  src={card.src} alt={card.name} id={card.id} />

5bde6ecd8cec9aea6385777e7f678a198839ed9a
Thibault Verin <thibaultverin@MacBook-Air-de-Thibault.local>
2021-04-18 00:41:33 +0200

    changed style from navbar component sccs to styled component

--- src/components/Style/StyledComponents.js
+++ src/components/Style/StyledComponents.js
@@ -1,5 +1,105 @@
 import styled from 'styled-components';
 
+// ---- Navbar
+export const NavbarContainer = styled.div`
+    height: 13vh;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    background-color: #24445c;
+    color: #ffffd4;
+    font-family: 'Patrick Hand SC';
+`
+
+export const NavbarLogo = styled.img`
+    width: 65px;
+    position: absolute;
+    top: 12px;
+    left: 15px;
+    cursor: pointer;
+`
+
+export const NavbarTitleContainer = styled.div`
+    width: 100%;
+    text-align: center;
+    padding-left: 25px;
+
+    @media screen and (min-width: 1024px) {
+        width: 33%;
+    }
+`
+export const NavbarTitle = styled.h1`
+    width: 70%;
+    font-size: 45px;
+    margin-left: 15%;
+    cursor:pointer;
+    &:hover {
+        transition-duration: 0.3s;
+        transform: scale(1.1);
+    }
+
+    @media screen and (min-width: 768px) {
+        display: block;
+        padding-left: 35px
+    }
+
+
+    @media screen and (min-width: 1024px) {
+        font-size: 50px;
+    }
+`
+
+export const NavbarBurgerContainer = styled.div`
+    display: none;
+
+    @media screen and (min-width: 768px) {
+        display: block;
+        padding-right: 30px
+    }
+
+    @media screen and (min-width: 1024px) {
+        display: none;
+    }
+`
+
+export const NavbarLinksContainer = styled.div`
+    display: none;
+
+    @media screen and (min-width: 1024px) {
+        display: flex;
+        justify-content: flex-start;
+    }
+`
+
+export const NavbarLinks = styled.input`
+    @media screen and (min-width: 1024px) {
+        color: #ffffd4;
+        font-family: 'Patrick Hand SC';
+        font-size: 35px;
+        font-weight: bolder;
+        background: transparent;
+        border: none;
+        margin: 0 30px;
+        padding: 0;
+        outline: none;
+        cursor: pointer;
+        &:hover {
+            transition-duration: 0.4s;
+            transform: scale(1.3);
+        }
+        &:first-child {
+            &:focus {
+                border:none
+            }
+        }
+        &:focus {
+            border-bottom: 2px solid #ffffd4;
+        }
+    }
+`
+
+
+// ---- Home Component
 export const HomeContainer = styled.div`
     height: 80%;
     display: flex;
@@ -10,8 +110,9 @@ export const HomeContainer = styled.div`
         height: 85%;
     }
 `
-// ---- Player Info Component
 
+
+// ---- Player Info Component
 export const PlayerInfoContainer = styled.div`
     width: 80%;
     height: 60%;
@@ -36,8 +137,9 @@ export const PlayerInfoMode = styled.h2`
     margin: 0 0 10px;
 `
 
-// ---- Player Number Component
 
+
+// ---- Player Number Component
 export const PlayerNumberContainer = styled.div`
     width: 80%;
     height: 60%;
